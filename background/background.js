44daf1bbee447a56cfc34d6e0124c91b9f883c06
wjy1919da <jiw421@lehigh.edu>
2023-03-31 23:32:50 -0400

    update_0331

--- background/background.js
+++ background/background.js
@@ -1,87 +1,14 @@
 let email;
 
-function insertdata(uid)
-{
-  //var insert_date=  new Date();
-  fetch("https://redditchrome.herokuapp.com/api/insert", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      
-      userid: uid,
-      user_action_onReddit:[],
-      browser_history:[]
-    })
-  })
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("Failed to insert data");
-    }
-  })
-  .then(data => {
-    console.log("Data inserted successfully:", data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
-  
-}
 
-// insert user action into db
-function insertUserAction(uid, action, target,action_date) {
-	//const insert_date = new Date();
-	console.log("uid: ",uid,"action: ",action,"target: ",target,"action_date: ",action_date)
-	fetch("https://redditchrome.herokuapp.com/api/updateUserAction", {
-	  method: "POST",
-	  headers: {
-		"Content-Type": "application/json"
-	  },
-	  body: JSON.stringify({
-		userid: uid,
-		user_action_onReddit: [{
-		  action_date: action_date,
-		  user_action: action,
-		  action_target: target
-		}]
-	  })
-	})
-	.then(response => {
-	  if (response.ok) {
-		return response.json();
-	  } else {
-		throw new Error("Failed to insert user action");
-	  }
-	})
-	.then(data => {
-	  console.log("User action inserted successfully:", data);
-	})
-	.catch(error => {
-	  console.error(error);
-	});
-  }
+test_addon_background("hello+++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
-function getKey(item){
-	console.log(item.isHighLight)
-}
-function onError(error) {
-  console.log(error)
-}
-// Asynchronous Method Invocation
-function loadStorage(key){
-	console.log("key is: ",key)
-	browser.storage.local.get(key).then(getAuth, onError);
-}
-// fake keywords list
-keywords = ["Wednesdays","What","Accessibility","Latest","dynamite","covid","Mask"]
 
-// replace content
-let dictionary = new Map();
-dictionary.set('Mask', '🚴');
-dictionary.set('covid', '💣');
+
+
+
+
+
 
 // sort map
 let tempArray = Array.from(dictionary);
